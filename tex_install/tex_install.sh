5662b24571a21c7de0b4018df9ce3455dd7c401a
Arita Yuta <k104009y@mail.kyutech.jp>
2015-03-16 14:45:44 +0900

    [Add] texをインストールするスクリプトを追加。

---
+++ tex_install/tex_install.sh
@@ -0,0 +1,3 @@
+wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+mkdir tex_install_script && tar xf install-tl-unx.tar.gz -C tex_install_script --strip-components 1
+sudo ./install-tl
